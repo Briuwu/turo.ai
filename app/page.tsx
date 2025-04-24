44e4271a3f1e84c12d111021645c8536a7be1388
Briuwu <millontebrian@gmail.com>
2025-04-24 09:55:55 +0800

    feat: wrap AI component in Suspense for improved loading handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { AI } from "@/components/AI";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="p-5 max-w-[1440px] mx-auto md:px-2 space-y-10 relative">
-      <AI />
+      <Suspense>
+        <AI />
+      </Suspense>
     </main>
   );
 }
